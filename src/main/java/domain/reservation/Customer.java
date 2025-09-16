bbd60f7f4bc527f569d6fd34816f12075309d50d
squareCaaat <danny79052@gmail.com>
2025-09-16 10:29:25 +0900

    feat: Customer 클래스 작성

---
+++ src/main/java/domain/reservation/Customer.java
@@ -0,0 +1,16 @@
+package domain.reservation;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+    private BigDecimal balance;
+    private MembershipPoint membershipPoint;
+    private List<Reservation> reservations = new ArrayList<>();
+
+    public Customer(BigDecimal balance) {
+        this.balance = balance;
+        membershipPoint = new MembershipPoint(BigDecimal.ZERO);
+    }
+}
