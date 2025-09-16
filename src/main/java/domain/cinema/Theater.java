3af5ca2e203a5d24fc0b17c6079ecd58b95f8773
squareCaaat <danny79052@gmail.com>
2025-09-17 00:45:05 +0900

    feat(Theater): 예약 가능 여부 검증 메서드 추가

--- src/main/java/domain/cinema/Theater.java
+++ src/main/java/domain/cinema/Theater.java
@@ -1,11 +1,46 @@
 package domain.cinema;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Theater {
+    private static int idCounter = 1;
+
+    private final int id;
     private List<Screen> screens = new ArrayList<>();
 
     public Theater() {
+        id = idCounter++;
+    }
+
+    public Boolean hasValidScreen(LocalDateTime when, Movie movie) {
+        for (Screen screen : screens) {
+            return screen.willBeScreening(when, movie);
+        }
+        return false;
+    }
+
+    public Boolean isAvailable(LocalDateTime when, Movie movie) {
+        if(!hasValidScreen(when, movie)) {
+            return false;
+        }
+        for (Screen screen : screens) {
+            return screen.hasEmptySeat();
+        }
+        return false;
+    }
+
+    public Screen getScreen(LocalDateTime when, Movie movie) {
+        for (Screen screen : screens) {
+            if (screen.willBeScreening(when, movie)) {
+                return screen;
+            }
+        }
+        return null;
+    }
+
+    public int getNumber() {
+        return id;
     }
 }
