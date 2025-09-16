4e567ddeffa404d24c6e6ebc717c35450fcb5fce
squareCaaat <danny79052@gmail.com>
2025-09-16 10:28:40 +0900

    feat: Theater 클래스 작성

---
+++ src/main/java/domain/cinema/Theater.java
@@ -0,0 +1,11 @@
+package domain.cinema;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Theater {
+    private List<Screen> screens = new ArrayList<>();
+
+    public Theater() {
+    }
+}
